9b4b03ad05bf377a9d3ec2c1cd172ea69f2d92b1
AkihiroYasunaga <Aki@Aki.local>
2015-06-02 10:56:17 +0900

    create Model

---
+++ app/src/main/java/model/BaseModel.java
@@ -0,0 +1,9 @@
+package model;
+
+/**
+ * Created by Aki on 15/06/02.
+ */
+public class BaseModel {
+    private static final String TAG = BaseModel.class.getSimpleName();
+    private final BaseModel self = this;
+}
---
+++ app/src/main/java/model/Task.java
@@ -0,0 +1,8 @@
+package model;
+
+/**
+ * Created by Aki on 15/06/02.
+ */
+public class Task {
+
+}
