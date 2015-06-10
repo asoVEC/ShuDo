2a99590c82bd06265402a491791ad631d667fe12
AkihiroYasunaga <Aki@Aki.local>
2015-06-10 13:54:21 +0900

    SQL関係のクラス作成

--- app/src/main/java/model/MySQLiteOpenHelper.java
+++ app/src/main/java/model/MySQLiteOpenHelper.java
@@ -12,7 +12,7 @@ public class MySQLiteOpenHelper extends SQLiteOpenHelper  {
     private static final String TAG = MySQLiteOpenHelper.class.getSimpleName();
     private final MySQLiteOpenHelper self = this;
     private final String CREATE_STATEMENT = "create table task (_id integer primary key autoincrement," +
-            "content text not null )";
+            "content text not null)";
     private final String INSERT_STATMENT = "insert into task (content) values('ますやま')";
 
     public MySQLiteOpenHelper(Context c){
