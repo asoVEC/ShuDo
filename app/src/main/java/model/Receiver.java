78b873c62bd73cc58d72513da5312b33f69c7b79
Shohe Masuyama <cisuco00@gmail.com>
2015-06-24 14:12:40 +0900

    tt
    tt

---
+++ app/src/main/java/model/Receiver.java
@@ -0,0 +1,17 @@
+package model;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class Receiver extends BroadcastReceiver {
+    public Receiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
